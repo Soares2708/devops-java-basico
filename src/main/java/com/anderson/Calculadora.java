99b2c7dc46baacb56a76dce7113dd990e7c28520
Soares2708 <andersonmhsc@gmail.com>
2026-02-25 13:12:30 -0300

    feat: adiciona método dividir com validação de erro

--- src/main/java/com/anderson/Calculadora.java
+++ src/main/java/com/anderson/Calculadora.java
@@ -13,4 +13,11 @@ public class Calculadora {
     public int multiplicar(int a, int b) {
         return a * b;
     }
+
+    public int dividir(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Não é possível dividir por zero");
+        }
+        return a / b;
+    }
 }
